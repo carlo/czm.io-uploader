725d3442590c57c47dc067bb0dc0c852599d03dc
Carlo Zottmann <carlo@municode.de>
2015-12-08 10:56:16 +0100

    [CHG] Output secure URL.

--- upload.sh
+++ upload.sh
@@ -6,12 +6,10 @@ set icon (dirname (status --current-filename))/icon.png
 if test -f $local_filename
   # Upload filename schema: `"[0-9,a-z]{12}\." + original extension`
   set upload_filename (uuidgen | cut -d "-" -f 1-1 | tr '[:upper:]' '[:lower:]').(basename $local_filename | sed 's/.*\.//g')
-  set url "http://czm.io/u/$upload_filename"
+  set url "https://czm.io/u/$upload_filename"
 
   echo $url | pbcopy
   rsync --protect-args --no-perms --no-owner --no-group --chmod=0644 "$local_filename" "jiffybox:sites/czm.io-uploads/$upload_filename"; and \
     /usr/local/bin/terminal-notifier -title "File finished uploading" -message "$url" -appIcon $icon -open "$url"; or \
     /usr/local/bin/terminal-notifier -title "File couldn't be uploaded" -message "$local_filename" -appIcon $icon
 end
-
-exit
